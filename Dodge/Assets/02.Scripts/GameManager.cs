448715ed25309f4c93fc1f71559fd3a4c3c39a5c
이승헌 <rltmsla5@naver.com>
2022-10-13 14:11:09 +0900

    1013
    1013Update

--- Dodge/Assets/02.Scripts/GameManager.cs
+++ Dodge/Assets/02.Scripts/GameManager.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 using UnityEngine.UI;
 // 씬 관련 라이브러리 사용
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     //게임오버 시 활성화할 텍스트 게임 오브젝트 
     public GameObject gameOverText;
     //생존 시간을 표시할 텍스트 컴포넌트
-    public Text timeText;
+    public TextMeshProUGUI timeText;
     //최고 기록을 표시할 텍스트 컴포넌트
-    public Text recordText;
-
+    public TextMeshProUGUI recordText;
     //실제 생존시간
     private float surviveTime;
     //게임오버 상태
@@ -38,6 +38,15 @@ public class GameManager : MonoBehaviour
             surviveTime += Time.deltaTime;
             //갱신한 생존 시간을 time text 컴포넌트를 사용해 표시
             timeText.text = "Time : " + (int)surviveTime;
+        }
+      else
+        {
+            // 게임 오버 상태에서 R키를누른 경우
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                // SampleScene 로드
+                SceneManager.LoadScene("SampleScene");
+            }
         }
     }
 
